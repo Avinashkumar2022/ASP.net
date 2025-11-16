d454d50d902330edc889dfc882e2d5dc7654fdcf
Avinash <avinashkumar200217@gmail.com>
2025-11-16 19:00:00 +0530

    new

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3f405144d56151edd686fd2541de98079d8a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
